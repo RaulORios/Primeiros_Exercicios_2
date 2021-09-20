bc0821e1f747872f7f9dfda1acb5812959aa763b
RaulORios <raul.orios89@outlook.com>
2021-09-20 14:50:56 -0300

    Arquivos atualizados

---
+++ src/Primeiros_Exercicios_2/Funcionarios.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primeiros_Exercicios_2
+{
+    class Funcionarios
+    {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ src/Primeiros_Exercicios_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiros_Exercicios_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Funcionarios funcionario1 = new Funcionarios();
+            Funcionarios funcionario2 = new Funcionarios();
+
+            Console.WriteLine("Dados do primeiro funcionario: ");
+            Console.Write("Nome: ");
+            funcionario1.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario1.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionario: ");
+            Console.Write("Nome: ");
+            funcionario2.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double mediaSalario = (funcionario1.salario + funcionario2.salario) / 2.0;
+
+            Console.WriteLine($"Sálario médio: {mediaSalario.ToString("F2",CultureInfo.InvariantCulture)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
